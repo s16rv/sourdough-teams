0882597ac34f4a6160485174b0ebb87d09fb3005
Miftahul Arifin <miftahul97@gmail.com>
2024-10-02 13:54:01 +0700

    test: add test template

---
+++ test/smart-account/signatureVerifier.test.ts
@@ -0,0 +1,15 @@
+import hre from "hardhat";
+import { expect } from "chai";
+import { SignatureVerifier } from "../../typechain-types";
+  
+describe("SignatureVerifier", function () {
+    let signatureVerifier: SignatureVerifier;
+    beforeEach(async function () {
+        const SignatureVerifier = await hre.ethers.getContractFactory("SignatureVerifier");
+        signatureVerifier = await SignatureVerifier.deploy();
+    });
+
+    it("Should set the right unlockTime", async function () {
+        expect(true).to.equal(true);
+    });
+});
