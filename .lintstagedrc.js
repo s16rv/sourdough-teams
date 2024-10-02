9254df1a1860ab11ab39022e02d6848dd93fabea
Miftahul Arifin <miftahul97@gmail.com>
2024-10-02 14:30:25 +0700

    chore: update style

--- .lintstagedrc.js
+++
@@ -1,5 +0,0 @@
-const prettierConfig = require('./prettier.config');
-
-module.exports = {
-  [prettierConfig.fileExtensions]: 'prettier --write .'
-};
--- test/smart-account/signatureVerifier.test.ts
+++ test/smart-account/signatureVerifier.test.ts
@@ -1,16 +1,16 @@
-import hre from "hardhat"
-import { expect } from "chai"
-import { SignatureVerifier } from "../../typechain-types"
+import hre from "hardhat";
+import { expect } from "chai";
+import { SignatureVerifier } from "../../typechain-types";
 
 describe("SignatureVerifier", function () {
-  let signatureVerifier: SignatureVerifier
-  beforeEach(async function () {
-    const SignatureVerifier =
-      await hre.ethers.getContractFactory("SignatureVerifier")
-    signatureVerifier = await SignatureVerifier.deploy()
-  })
+    let signatureVerifier: SignatureVerifier;
+    beforeEach(async function () {
+        const SignatureVerifier =
+            await hre.ethers.getContractFactory("SignatureVerifier");
+        signatureVerifier = await SignatureVerifier.deploy();
+    });
 
-  it("Should set the right unlockTime", async function () {
-    expect(true).to.equal(true)
-  })
-})
+    it("Should set the right unlockTime", async function () {
+        expect(true).to.equal(true);
+    });
+});
