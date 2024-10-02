91661e8a118f9674c0fd01cd9f9263a4bc3a107c
Miftahul Arifin <miftahul97@gmail.com>
2024-10-02 14:07:07 +0700

    chore: setup prettier

--- test/smart-account/signatureVerifier.test.ts
+++ test/smart-account/signatureVerifier.test.ts
@@ -1,15 +1,16 @@
-import hre from "hardhat";
-import { expect } from "chai";
-import { SignatureVerifier } from "../../typechain-types";
-  
-describe("SignatureVerifier", function () {
-    let signatureVerifier: SignatureVerifier;
-    beforeEach(async function () {
-        const SignatureVerifier = await hre.ethers.getContractFactory("SignatureVerifier");
-        signatureVerifier = await SignatureVerifier.deploy();
-    });
+import hre from "hardhat"
+import { expect } from "chai"
+import { SignatureVerifier } from "../../typechain-types"
 
-    it("Should set the right unlockTime", async function () {
-        expect(true).to.equal(true);
-    });
-});
+describe("SignatureVerifier", function () {
+  let signatureVerifier: SignatureVerifier
+  beforeEach(async function () {
+    const SignatureVerifier =
+      await hre.ethers.getContractFactory("SignatureVerifier")
+    signatureVerifier = await SignatureVerifier.deploy()
+  })
+
+  it("Should set the right unlockTime", async function () {
+    expect(true).to.equal(true)
+  })
+})
