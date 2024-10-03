7d9b7d1a34d25d956e02545a89fdd1da3ca9576f
Miftahul Arifin <miftahul97@gmail.com>
2024-10-03 13:56:45 +0700

    fix: format

--- test/smart-account/account.test.ts
+++ test/smart-account/account.test.ts
@@ -1,18 +1,20 @@
 import hre from "hardhat";
 import { expect } from "chai";
 import { ethers, parseEther, sha256 } from "ethers";
-import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers"
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
 import { Account } from "../../typechain-types";
 
-const TX_BYTES_A_1 = "CoECCv4BCiUvaW50ZXJjaGFpbmF1dGguaWNhdXRoLnYxLk1zZ1N1Ym1pdFR4EtQBCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSCWNoYW5uZWwtMBqQAQocL2Nvc21vcy5iYW5rLnYxYmV0YTEuTXNnU2VuZBJwCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSLWNvc21vczFtZ3A3cW52dW1obGNxNmU0ejJxMDVyMjlkY2hjeXVnMHozenhlZxoQCgV1YmV0YRIHMjAwMDAwMCCAoPHCsTQSWApQCkYKHy9jb3Ntb3MuY3J5cHRvLnNlY3AyNTZrMS5QdWJLZXkSIwohApC+f+iGx0i+gOmLNA0UGNC/54ZWde5ZfZ2FBSZSAIXwEgQKAggBGAESBBDAmgwaB2FscGhhLTE="
-const TX_BYTES_A_2 = "CoECCv4BCiUvaW50ZXJjaGFpbmF1dGguaWNhdXRoLnYxLk1zZ1N1Ym1pdFR4EtQBCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSCWNoYW5uZWwtMBqQAQocL2Nvc21vcy5iYW5rLnYxYmV0YTEuTXNnU2VuZBJwCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSLWNvc21vczFtZ3A3cW52dW1obGNxNmU0ejJxMDVyMjlkY2hjeXVnMHozenhlZxoQCgV1YmV0YRIHMjAwMDAwMCCAoPHCsTQSWApQCkYKHy9jb3Ntb3MuY3J5cHRvLnNlY3AyNTZrMS5QdWJLZXkSIwohApC+f+iGx0i+gOmLNA0UGNC/54ZWde5ZfZ2FBSZSAIXwEgQKAggBGAISBBDAmgwaB2FscGhhLTE="
-const SIGNATURE_A_1 = "GHUe6rGUxcUuzLnjYJ8qE+qYoHkTjcKdr2c5Ea4mCJlzrMvDi6sZZCNO4K6taTiaeYmMgL+MNXBjMinM9fJKHg=="
-const SIGNATURE_A_2 = "0A5QeQgokPkkOxxYgdw+shCF+3nwr1eq4fxhax+kQ+J3TycL0JpeF4CuSDyvnUHwGMQk4ecx/15cl9CzJtUbgw=="
+const TX_BYTES_A_1 =
+    "CoECCv4BCiUvaW50ZXJjaGFpbmF1dGguaWNhdXRoLnYxLk1zZ1N1Ym1pdFR4EtQBCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSCWNoYW5uZWwtMBqQAQocL2Nvc21vcy5iYW5rLnYxYmV0YTEuTXNnU2VuZBJwCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSLWNvc21vczFtZ3A3cW52dW1obGNxNmU0ejJxMDVyMjlkY2hjeXVnMHozenhlZxoQCgV1YmV0YRIHMjAwMDAwMCCAoPHCsTQSWApQCkYKHy9jb3Ntb3MuY3J5cHRvLnNlY3AyNTZrMS5QdWJLZXkSIwohApC+f+iGx0i+gOmLNA0UGNC/54ZWde5ZfZ2FBSZSAIXwEgQKAggBGAESBBDAmgwaB2FscGhhLTE=";
+const TX_BYTES_A_2 =
+    "CoECCv4BCiUvaW50ZXJjaGFpbmF1dGguaWNhdXRoLnYxLk1zZ1N1Ym1pdFR4EtQBCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSCWNoYW5uZWwtMBqQAQocL2Nvc21vcy5iYW5rLnYxYmV0YTEuTXNnU2VuZBJwCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSLWNvc21vczFtZ3A3cW52dW1obGNxNmU0ejJxMDVyMjlkY2hjeXVnMHozenhlZxoQCgV1YmV0YRIHMjAwMDAwMCCAoPHCsTQSWApQCkYKHy9jb3Ntb3MuY3J5cHRvLnNlY3AyNTZrMS5QdWJLZXkSIwohApC+f+iGx0i+gOmLNA0UGNC/54ZWde5ZfZ2FBSZSAIXwEgQKAggBGAISBBDAmgwaB2FscGhhLTE=";
+const SIGNATURE_A_1 = "GHUe6rGUxcUuzLnjYJ8qE+qYoHkTjcKdr2c5Ea4mCJlzrMvDi6sZZCNO4K6taTiaeYmMgL+MNXBjMinM9fJKHg==";
+const SIGNATURE_A_2 = "0A5QeQgokPkkOxxYgdw+shCF+3nwr1eq4fxhax+kQ+J3TycL0JpeF4CuSDyvnUHwGMQk4ecx/15cl9CzJtUbgw==";
 
-const EXPECTED_SIGNER = "0x07557D755E777B85d878D34861cd52126524a155"
-const ENTRYPOINT_ADDRESS = "0x3bd70e10d71c6e882e3c1809d26a310d793646eb"
-const RECIPIENT_ADDRESS = "0xaa25Aa7a19f9c426E07dee59b12f944f4d9f1DD3"
+const EXPECTED_SIGNER = "0x07557D755E777B85d878D34861cd52126524a155";
+const ENTRYPOINT_ADDRESS = "0x3bd70e10d71c6e882e3c1809d26a310d793646eb";
+const RECIPIENT_ADDRESS = "0xaa25Aa7a19f9c426E07dee59b12f944f4d9f1DD3";
 
 describe("Account", function () {
     let account: Account;
@@ -25,7 +27,7 @@ describe("Account", function () {
         const Account = await hre.ethers.getContractFactory("Account");
         account = await Account.deploy(recover.address, EXPECTED_SIGNER, ENTRYPOINT_ADDRESS);
 
-        const accountAddr = await account.getAddress()
+        const accountAddr = await account.getAddress();
 
         await recover.sendTransaction({
             to: accountAddr,
@@ -34,44 +36,44 @@ describe("Account", function () {
     });
 
     it("Should have funds", async function () {
-        const accountAddr = await account.getAddress()
-        const balance = await hre.ethers.provider.getBalance(accountAddr)
-        
+        const accountAddr = await account.getAddress();
+        const balance = await hre.ethers.provider.getBalance(accountAddr);
+
         expect(balance).to.gt(0);
     });
 
     it("Should validate operation", async function () {
-        const message = Buffer.from(TX_BYTES_A_1, 'base64');
-        const signature = Buffer.from(SIGNATURE_A_1, 'base64');
+        const message = Buffer.from(TX_BYTES_A_1, "base64");
+        const signature = Buffer.from(SIGNATURE_A_1, "base64");
 
-        const r = '0x' + signature.subarray(0, 32).toString('hex');
-        const s = '0x' + signature.subarray(32, 64).toString('hex');
+        const r = "0x" + signature.subarray(0, 32).toString("hex");
+        const s = "0x" + signature.subarray(32, 64).toString("hex");
 
         const messageHash = sha256(message);
-        const isValid = await account.validateOperation(messageHash, r, s)
+        const isValid = await account.validateOperation(messageHash, r, s);
         expect(isValid).to.be.true;
     });
 
     it("Should not validate operation", async function () {
-        const message = Buffer.from(TX_BYTES_A_2, 'base64');
-        const signature = Buffer.from(SIGNATURE_A_1, 'base64');
+        const message = Buffer.from(TX_BYTES_A_2, "base64");
+        const signature = Buffer.from(SIGNATURE_A_1, "base64");
 
-        const r = '0x' + signature.subarray(0, 32).toString('hex');
-        const s = '0x' + signature.subarray(32, 64).toString('hex');
+        const r = "0x" + signature.subarray(0, 32).toString("hex");
+        const s = "0x" + signature.subarray(32, 64).toString("hex");
 
         const messageHash = sha256(message);
-        const isValid = await account.validateOperation(messageHash, r, s)
+        const isValid = await account.validateOperation(messageHash, r, s);
         expect(isValid).to.be.false;
     });
 
     it("Should execute transaction using recover account", async function () {
-        const amountToSend = parseEther("0.001")
+        const amountToSend = parseEther("0.001");
         const initialRecipientBalance = await hre.ethers.provider.getBalance(RECIPIENT_ADDRESS);
-    
+
         await expect(account.connect(recover).executeTransaction(RECIPIENT_ADDRESS, amountToSend, "0x"))
             .to.emit(account, "TransactionExecuted")
             .withArgs(RECIPIENT_ADDRESS, amountToSend, "0x");
-    
+
         const finalRecipientBalance = await hre.ethers.provider.getBalance(RECIPIENT_ADDRESS);
         expect(finalRecipientBalance).to.equal(initialRecipientBalance + amountToSend);
     });
@@ -79,10 +81,11 @@ describe("Account", function () {
     it("Should not execute transaction using stranger account", async function () {
         const amountToSend = parseEther("0.001");
         const initialRecipientBalance = await hre.ethers.provider.getBalance(RECIPIENT_ADDRESS);
-    
-        await expect(account.connect(stranger).executeTransaction(RECIPIENT_ADDRESS, amountToSend, "0x"))
-            .to.be.revertedWithCustomError(account, "NotEntryPointOrRecover");
-    
+
+        await expect(
+            account.connect(stranger).executeTransaction(RECIPIENT_ADDRESS, amountToSend, "0x")
+        ).to.be.revertedWithCustomError(account, "NotEntryPointOrRecover");
+
         const finalRecipientBalance = await hre.ethers.provider.getBalance(RECIPIENT_ADDRESS);
         expect(finalRecipientBalance).to.equal(initialRecipientBalance);
     });
--- test/smart-account/signatureVerifier.test.ts
+++ test/smart-account/signatureVerifier.test.ts
@@ -3,13 +3,15 @@ import { expect } from "chai";
 import { sha256 } from "ethers";
 import { SignatureVerifier } from "../../typechain-types";
 
-const TX_BYTES_A_1 = "CoECCv4BCiUvaW50ZXJjaGFpbmF1dGguaWNhdXRoLnYxLk1zZ1N1Ym1pdFR4EtQBCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSCWNoYW5uZWwtMBqQAQocL2Nvc21vcy5iYW5rLnYxYmV0YTEuTXNnU2VuZBJwCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSLWNvc21vczFtZ3A3cW52dW1obGNxNmU0ejJxMDVyMjlkY2hjeXVnMHozenhlZxoQCgV1YmV0YRIHMjAwMDAwMCCAoPHCsTQSWApQCkYKHy9jb3Ntb3MuY3J5cHRvLnNlY3AyNTZrMS5QdWJLZXkSIwohApC+f+iGx0i+gOmLNA0UGNC/54ZWde5ZfZ2FBSZSAIXwEgQKAggBGAESBBDAmgwaB2FscGhhLTE="
-const TX_BYTES_A_2 = "CoECCv4BCiUvaW50ZXJjaGFpbmF1dGguaWNhdXRoLnYxLk1zZ1N1Ym1pdFR4EtQBCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSCWNoYW5uZWwtMBqQAQocL2Nvc21vcy5iYW5rLnYxYmV0YTEuTXNnU2VuZBJwCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSLWNvc21vczFtZ3A3cW52dW1obGNxNmU0ejJxMDVyMjlkY2hjeXVnMHozenhlZxoQCgV1YmV0YRIHMjAwMDAwMCCAoPHCsTQSWApQCkYKHy9jb3Ntb3MuY3J5cHRvLnNlY3AyNTZrMS5QdWJLZXkSIwohApC+f+iGx0i+gOmLNA0UGNC/54ZWde5ZfZ2FBSZSAIXwEgQKAggBGAISBBDAmgwaB2FscGhhLTE="
-const SIGNATURE_A_1 = "GHUe6rGUxcUuzLnjYJ8qE+qYoHkTjcKdr2c5Ea4mCJlzrMvDi6sZZCNO4K6taTiaeYmMgL+MNXBjMinM9fJKHg=="
-const SIGNATURE_A_2 = "0A5QeQgokPkkOxxYgdw+shCF+3nwr1eq4fxhax+kQ+J3TycL0JpeF4CuSDyvnUHwGMQk4ecx/15cl9CzJtUbgw=="
+const TX_BYTES_A_1 =
+    "CoECCv4BCiUvaW50ZXJjaGFpbmF1dGguaWNhdXRoLnYxLk1zZ1N1Ym1pdFR4EtQBCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSCWNoYW5uZWwtMBqQAQocL2Nvc21vcy5iYW5rLnYxYmV0YTEuTXNnU2VuZBJwCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSLWNvc21vczFtZ3A3cW52dW1obGNxNmU0ejJxMDVyMjlkY2hjeXVnMHozenhlZxoQCgV1YmV0YRIHMjAwMDAwMCCAoPHCsTQSWApQCkYKHy9jb3Ntb3MuY3J5cHRvLnNlY3AyNTZrMS5QdWJLZXkSIwohApC+f+iGx0i+gOmLNA0UGNC/54ZWde5ZfZ2FBSZSAIXwEgQKAggBGAESBBDAmgwaB2FscGhhLTE=";
+const TX_BYTES_A_2 =
+    "CoECCv4BCiUvaW50ZXJjaGFpbmF1dGguaWNhdXRoLnYxLk1zZ1N1Ym1pdFR4EtQBCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSCWNoYW5uZWwtMBqQAQocL2Nvc21vcy5iYW5rLnYxYmV0YTEuTXNnU2VuZBJwCi1jb3Ntb3MxenlwcWE3NmplN3B4c2R3a2ZhaDZtdTlhNTgzc2p1NnhxdDNtdjYSLWNvc21vczFtZ3A3cW52dW1obGNxNmU0ejJxMDVyMjlkY2hjeXVnMHozenhlZxoQCgV1YmV0YRIHMjAwMDAwMCCAoPHCsTQSWApQCkYKHy9jb3Ntb3MuY3J5cHRvLnNlY3AyNTZrMS5QdWJLZXkSIwohApC+f+iGx0i+gOmLNA0UGNC/54ZWde5ZfZ2FBSZSAIXwEgQKAggBGAISBBDAmgwaB2FscGhhLTE=";
+const SIGNATURE_A_1 = "GHUe6rGUxcUuzLnjYJ8qE+qYoHkTjcKdr2c5Ea4mCJlzrMvDi6sZZCNO4K6taTiaeYmMgL+MNXBjMinM9fJKHg==";
+const SIGNATURE_A_2 = "0A5QeQgokPkkOxxYgdw+shCF+3nwr1eq4fxhax+kQ+J3TycL0JpeF4CuSDyvnUHwGMQk4ecx/15cl9CzJtUbgw==";
 
-const EXPECTED_SIGNER = "0x07557D755E777B85d878D34861cd52126524a155"
-const SIGNATURE_V = 27
+const EXPECTED_SIGNER = "0x07557D755E777B85d878D34861cd52126524a155";
+const SIGNATURE_V = 27;
 
 describe("SignatureVerifier", function () {
     let signatureVerifier: SignatureVerifier;
@@ -19,50 +21,50 @@ describe("SignatureVerifier", function () {
     });
 
     it("Should recover signer", async function () {
-        const message = Buffer.from(TX_BYTES_A_1, 'base64');
-        const signature = Buffer.from(SIGNATURE_A_1, 'base64');
+        const message = Buffer.from(TX_BYTES_A_1, "base64");
+        const signature = Buffer.from(SIGNATURE_A_1, "base64");
 
-        const r = '0x' + signature.subarray(0, 32).toString('hex');
-        const s = '0x' + signature.subarray(32, 64).toString('hex');
+        const r = "0x" + signature.subarray(0, 32).toString("hex");
+        const s = "0x" + signature.subarray(32, 64).toString("hex");
 
         const messageHash = sha256(message);
-        const signer = await signatureVerifier.recoverSigner(messageHash, r, s, SIGNATURE_V)
+        const signer = await signatureVerifier.recoverSigner(messageHash, r, s, SIGNATURE_V);
         expect(signer).to.equal(EXPECTED_SIGNER);
     });
 
     it("Should verify signature A", async function () {
-        const message = Buffer.from(TX_BYTES_A_1, 'base64');
-        const signature = Buffer.from(SIGNATURE_A_1, 'base64');
+        const message = Buffer.from(TX_BYTES_A_1, "base64");
+        const signature = Buffer.from(SIGNATURE_A_1, "base64");
 
-        const r = '0x' + signature.subarray(0, 32).toString('hex');
-        const s = '0x' + signature.subarray(32, 64).toString('hex');
+        const r = "0x" + signature.subarray(0, 32).toString("hex");
+        const s = "0x" + signature.subarray(32, 64).toString("hex");
 
         const messageHash = sha256(message);
-        const isValid = await signatureVerifier.verifySignature(messageHash, r, s, SIGNATURE_V, EXPECTED_SIGNER)
+        const isValid = await signatureVerifier.verifySignature(messageHash, r, s, SIGNATURE_V, EXPECTED_SIGNER);
         expect(isValid).to.be.true;
     });
 
     it("Should verify signature B", async function () {
-        const message = Buffer.from(TX_BYTES_A_2, 'base64');
-        const signature = Buffer.from(SIGNATURE_A_2, 'base64');
+        const message = Buffer.from(TX_BYTES_A_2, "base64");
+        const signature = Buffer.from(SIGNATURE_A_2, "base64");
 
-        const r = '0x' + signature.subarray(0, 32).toString('hex');
-        const s = '0x' + signature.subarray(32, 64).toString('hex');
+        const r = "0x" + signature.subarray(0, 32).toString("hex");
+        const s = "0x" + signature.subarray(32, 64).toString("hex");
 
         const messageHash = sha256(message);
-        const isValid = await signatureVerifier.verifySignature(messageHash, r, s, SIGNATURE_V, EXPECTED_SIGNER)
+        const isValid = await signatureVerifier.verifySignature(messageHash, r, s, SIGNATURE_V, EXPECTED_SIGNER);
         expect(isValid).to.be.true;
     });
 
     it("Should not verify signature", async function () {
-        const message = Buffer.from(TX_BYTES_A_1, 'base64');
-        const signature = Buffer.from(SIGNATURE_A_2, 'base64');
+        const message = Buffer.from(TX_BYTES_A_1, "base64");
+        const signature = Buffer.from(SIGNATURE_A_2, "base64");
 
-        const r = '0x' + signature.subarray(0, 32).toString('hex');
-        const s = '0x' + signature.subarray(32, 64).toString('hex');
+        const r = "0x" + signature.subarray(0, 32).toString("hex");
+        const s = "0x" + signature.subarray(32, 64).toString("hex");
 
         const messageHash = sha256(message);
-        const isValid = await signatureVerifier.verifySignature(messageHash, r, s, SIGNATURE_V, EXPECTED_SIGNER)
+        const isValid = await signatureVerifier.verifySignature(messageHash, r, s, SIGNATURE_V, EXPECTED_SIGNER);
         expect(isValid).to.be.false;
     });
 });
