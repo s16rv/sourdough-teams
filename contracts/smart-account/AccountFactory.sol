97e038d4191354286565baebfc45779c8efddbfa
Miftahul Arifin <miftahul97@gmail.com>
2024-10-04 13:47:37 +0700

    feat: add comment account factory

--- contracts/smart-account/AccountFactory.sol
+++ contracts/smart-account/AccountFactory.sol
@@ -8,10 +8,21 @@ import "./account/Account.sol";
 import "hardhat/console.sol";
 
 contract AccountFactory is IAccountFactory, SignatureVerifier {
+    // Mapping of signers to their list of created accounts
     mapping(address => address[]) public signerAccounts;
 
     uint8 private constant SIGNATURE_V = 27;
 
+    /**
+     * @dev Creates a new account contract using a signature for verification. 
+     *      The account is deployed using the CREATE2 opcode for address predictability.
+     * @param recover The address with recovery rights for the account.
+     * @param entryPoint The address of the entry point contract.
+     * @param messageHash The hash of the message to verify the signer's identity.
+     * @param r The r part of the signature.
+     * @param s The s part of the signature.
+     * @return accountAddress The address of the newly created account contract.
+     */
     function createAccount(
         address recover, 
         address entryPoint,
@@ -23,20 +34,34 @@ contract AccountFactory is IAccountFactory, SignatureVerifier {
         uint256 salt = uint256(keccak256(abi.encodePacked(signer, signerAccounts[signer].length)));
         address accountAddress = _deployAccount(recover, signer, entryPoint, salt);
 
+        // Store the new account for the signer
         signerAccounts[signer].push(accountAddress);
 
         emit AccountCreated(signer, accountAddress);
         return accountAddress;
     }
 
-    function _deployAccount(address recover, address signer, address entryPoint, uint256 salt) internal returns (address) {
-        // Define the bytecode of the account contract
+    /**
+     * @dev Deploys the account contract using the CREATE2 opcode for address predictability.
+     * @param recover The address with recovery rights for the account.
+     * @param signer The address of the signer, which will be tied to the new account.
+     * @param entryPoint The address of the entry point contract.
+     * @param salt The salt used for deterministic contract deployment with CREATE2.
+     * @return accountAddress The address of the newly deployed account contract.
+     */
+    function _deployAccount(
+        address recover,
+        address signer,
+        address entryPoint,
+        uint256 salt
+    ) internal returns (address) {
+        // Encode the creation bytecode of the Account contract
         bytes memory bytecode = abi.encodePacked(
             type(Account).creationCode,
             abi.encode(recover, signer, entryPoint)
         );
         
-        // Use CREATE2 to deploy the contract at a predictable address
+        // Use CREATE2 to deploy the contract with the provided salt
         address accountAddress;
         assembly {
             accountAddress := create2(0, add(bytecode, 0x20), mload(bytecode), salt)
@@ -47,8 +72,20 @@ contract AccountFactory is IAccountFactory, SignatureVerifier {
         return accountAddress;
     }
 
-    // Helper function to compute the address where the contract will be deployed
-    function computeAddress(address recover, address signer, address entryPoint, uint256 salt) external view returns (address) {
+    /**
+     * @dev Computes the address of an account contract to be deployed using CREATE2, without actually deploying it.
+     * @param recover The address with recovery rights for the account.
+     * @param signer The address of the signer for the account.
+     * @param entryPoint The address of the entry point contract.
+     * @param salt The salt used for deterministic contract deployment with CREATE2.
+     * @return The address at which the contract would be deployed.
+     */
+    function computeAddress(
+        address recover,
+        address signer,
+        address entryPoint,
+        uint256 salt
+    ) external view returns (address) {
         bytes memory bytecode = abi.encodePacked(
             type(Account).creationCode,
             abi.encode(recover, signer, entryPoint)
@@ -59,7 +96,11 @@ contract AccountFactory is IAccountFactory, SignatureVerifier {
         return address(uint160(uint256(hash)));
     }
 
-    // Function to get the list of accounts created by a user
+    /**
+     * @dev Returns the list of accounts created by a particular signer.
+     * @param signer The address of the signer whose accounts to retrieve.
+     * @return An array of account addresses created by the signer.
+     */
     function getAccounts(address signer) external view returns (address[] memory) {
         return signerAccounts[signer];
     }
--- contracts/smart-account/interfaces/IAccountFactory.sol
+++ contracts/smart-account/interfaces/IAccountFactory.sol
@@ -2,20 +2,27 @@
 pragma solidity ^0.8.9;
 
 interface IAccountFactory {
-    // Errors
+
+    /**
+     * @dev Error thrown when the account deployment fails.
+     */
     error FailedDeployAccount();
 
-    // Events
+    /**
+     * @dev Event emitted when a new account is created.
+     * @param signer The address of the signer who owns the account.
+     * @param accountAddress The address of the newly created account.
+     */
     event AccountCreated(address indexed signer, address indexed accountAddress);
 
     /**
-     * @notice Create a new account and return its address.
-     * @param recover Address of the account recovery.
-     * @param entryPoint Address of the account recovery.
-     * @param messageHash Hash of the message being signed.
-     * @param r ECDSA signature parameter r.
-     * @param s ECDSA signature parameter s.
-     * @return accountAddress The address of the created account.
+     * @dev Creates a new account contract using the provided parameters and deploys it using CREATE2.
+     * @param recover The address with recovery rights for the account.
+     * @param entryPoint The address of the entry point contract.
+     * @param messageHash The hash of the message to verify the signer's identity.
+     * @param r The r part of the signature.
+     * @param s The s part of the signature.
+     * @return accountAddress The address of the newly created account contract.
      */
     function createAccount(
         address recover, 
@@ -23,9 +30,27 @@ interface IAccountFactory {
         bytes32 messageHash,
         bytes32 r,
         bytes32 s
-    ) external returns (address accountAddress);
+    ) external returns (address);
 
-    function computeAddress(address recover, address signer, address entryPoint, uint256 salt) external view returns (address);
+    /**
+     * @dev Computes the address of an account contract that would be deployed using CREATE2, without actually deploying it.
+     * @param recover The address with recovery rights for the account.
+     * @param signer The address of the signer for the account.
+     * @param entryPoint The address of the entry point contract.
+     * @param salt The salt used for deterministic contract deployment with CREATE2.
+     * @return The address at which the contract would be deployed.
+     */
+    function computeAddress(
+        address recover,
+        address signer,
+        address entryPoint,
+        uint256 salt
+    ) external view returns (address);
 
+    /**
+     * @dev Returns the list of accounts created by a particular signer.
+     * @param signer The address of the signer whose accounts to retrieve.
+     * @return An array of account addresses created by the signer.
+     */
     function getAccounts(address signer) external view returns (address[] memory);
 }
