83114799bfc13ba200684be3aa2b0a72d870f693
Miftahul Arifin <miftahul97@gmail.com>
2025-12-18 14:05:48 +0700

    feat: Add custom test ERC20 token and a deployment script for TestUSDC.

--- contracts/testing-contracts/erc20/MyToken.sol
+++ contracts/testing-contracts/erc20/MyToken.sol
@@ -1,16 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import "./ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor(string memory name, string memory symbol, uint8 decimals)
-        ERC20(name, symbol, decimals)
-    {
-        // Mint 100 tokens to msg.sender
+    uint8 private _decimals;
+
+    constructor(string memory name, string memory symbol, uint8 decimals_) ERC20(name, symbol) {
+        _decimals = decimals_;
+        // Mint 100000 tokens to msg.sender
         // Similar to how
         // 1 dollar = 100 cents
         // 1 token = 1 * (10 ** decimals)
-        _mint(msg.sender, 100 * 10 ** uint256(decimals));
+        _mint(msg.sender, 100000 * 10 ** uint256(decimals_));
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
     }
 }
